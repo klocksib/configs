038628ffc9d2556b6931e66b53b589615d30e8f8
Brad Klocksiem <brad.klocksiem@bigfishgames.com>
2016-02-23 15:26:33 -0800

    updates

--- init.el
+++ init.el
@@ -55,6 +55,7 @@
     noctilux-theme
     solarized-theme
     sublime-themes
+    zerodark-theme
     zenburn-theme))
 
 (message "%s" "Refreshing package database...")
@@ -65,10 +66,22 @@
     (package-install pkg)))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("badc4f9ae3ee82a5ca711f3fd48c3f49ebe20e6303bba1912d4e2d19dd60ec98" default))))
-(custom-set-faces)
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "38ba6a938d67a452aeb1dada9d7cdeca4d9f18114e9fc8ed2b972573138d4664" "bc40f613df8e0d8f31c5eb3380b61f587e1b5bc439212e03d4ea44b26b4f408a" "ad950f1b1bf65682e390f3547d479fd35d8c66cafa2b8aa28179d78122faa947" "8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "badc4f9ae3ee82a5ca711f3fd48c3f49ebe20e6303bba1912d4e2d19dd60ec98" default)))
+ '(package-selected-packages
+   (quote
+    (zerodark-theme soothe-theme ample-zen-theme smyx-theme zenburn-theme yaml-mode writegood-mode web-mode sublime-themes solarized-theme sly rvm puppet-mode php-mode nodejs-repl noctilux-theme monokai-theme molokai-theme markdown-mode magit json-mode htmlize haskell-mode gruvbox-theme graphviz-dot-mode go-mode gist geiser flymake-python-pyflakes flycheck-pyflakes flycheck-perl6 flycheck-ocaml flycheck-clangcheck feature-mode erlang elpy deft csharp-mode coffee-mode clojure-mode ample-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 
 ;; GUI SETTINGS
@@ -78,12 +91,35 @@
 (show-paren-mode t)
 (global-flycheck-mode t)
 (setq indicate-empty-lines t)
-(load-theme 'gruvbox)
+(load-theme 'monokai)
 
 (cond ((eq system-type 'darwin)
        (message "%s" "Loading OSX specific settings.")
        (setq ns-use-srgb-colorspace t)
-       (set-frame-font "Menlo 11"))
+       (set-frame-font "Fira Code 11")
+       (let ((alist '((33 . ".\\(?:\\(?:==\\)\\|[!=]\\)")
+		      (35 . ".\\(?:[(?[_{]\\)")
+		      (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+		      (42 . ".\\(?:\\(?:\\*\\*\\)\\|[*/]\\)")
+		      (43 . ".\\(?:\\(?:\\+\\+\\)\\|\\+\\)")
+		      (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+		      (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=]\\)")
+		      (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+		      (58 . ".\\(?:[:=]\\)")
+		      (59 . ".\\(?:;\\)")
+		      (60 . ".\\(?:\\(?:!--\\)\\|\\(?:\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[/<=>|-]\\)")
+		      (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+		      (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+		      (63 . ".\\(?:[:=?]\\)")
+		      (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+		      (94 . ".\\(?:=\\)")
+		      (123 . ".\\(?:-\\)")
+		      (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+		      (126 . ".\\(?:[=@~-]\\)")
+		      )))
+	 (dolist (char-regexp alist)
+	   (set-char-table-range composition-function-table (car char-regexp)
+				 `([,(cdr char-regexp) 0 font-shape-gstring])))))
       ((or
         (eq system-type 'gnu/linux)
         (eq system-type 'berkeley-unix))
@@ -102,6 +138,8 @@
 
 ;; GENERAL SETTINGS
 ;; -----------------------------------------------------------------------------
+(setq user-full-name "Brad Klocksiem")
+(setq user-mail-address "brad.klocksiem@bigfishgames.com")
 (setq gc-cons-threshold 100000000)
 (setq initial-scratch-message "")
 (setq display-time-day-and-date t)ex
